5f8c34451e930de9ed54245dfaf6b69cdcc8db94
tommyrunner <1223758238@qq.com>
2025-04-25 09:12:39 +0800

    chore: 更新项目版本至1.1.7并完善指令导出
    - 将 package-lock.json 中的版本号更新为 1.1.7，确保版本一致性
    - 在 src/index.ts 中移除不再使用的指令导出
    - 在 src/directive/index.ts 中新增指令安装功能，简化指令注册逻辑
    - 更新测试文件，注册指令以便于测试组件使用

--- src/directive/index.ts
+++ src/directive/index.ts
@@ -1,2 +1,11 @@
-/** 导出指令 */
-export * from "./vLoading";
+import { App } from "vue";
+import { vLoading } from "./vLoading";
+
+/** 安装全部指令 */
+export const install = {
+  install: (app: App) => {
+    app.directive("loading", vLoading);
+  }
+};
+
+export default install;
